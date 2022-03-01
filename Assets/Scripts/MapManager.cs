a68b00e71758781ccec5edf8c73b93b92e89b2b7
Thinh Do <dqthinh69752@gmail.com>
2022-03-02 00:22:45 +0700

    Separate hex tile's type
    Use different prefab for different hex tile based on corresponding character's role (defense, attack, barrier/none)

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
+    public GameObject tilesGroup;
     [SerializeField]
-    private GameObject tilePrefab;
-    public int ring = 2;
+    private GameObject defenseTilePrefab;
+    [SerializeField]
+    private GameObject attackTilePrefab;
+    [SerializeField]
+    private GameObject barrierTilePrefab;
+    public int ring = 4;
 
     private HexGrid grid;
     private int minRing = 4;
@@ -35,19 +40,60 @@ public class MapManager : MonoBehaviour
 
     private bool GenerateTiles()
     {
-        if (!tilePrefab) return false;
+        // Center - use defense tile (always)
+        if (defenseTilePrefab)
+        {
+            GameObject centerObject = Instantiate(defenseTilePrefab, HexGrid.center, Quaternion.identity);
 
-        // Center
-        Instantiate(tilePrefab, HexGrid.center, Quaternion.identity);
+            // Add to group
+            if (tilesGroup)
+            {
+                centerObject.transform.parent = tilesGroup.transform;
+            }
+        }
+
+        int numGeneratedRings = ring > minRing ? ring : minRing;
+
+        // Determine number of rings for each prefab type: defense, attack, barrier
+        int numBarrierRings = 1;     // default;
+        int numDefenseRings = (int)((numGeneratedRings - numBarrierRings) / 2);
+        int numAttackRings = numGeneratedRings - numBarrierRings - numDefenseRings;
 
         // Rings
-        for (int i = 1, limit = ring > minRing ? ring : minRing; i <= limit; ++i)
+        for (int i = 1; i <= numGeneratedRings; ++i)
         {
+            // Decide which tile prefab to be used for the current ring
+            GameObject usedTilePrefab = barrierTilePrefab;
+            if (numDefenseRings > 0)
+            {
+                usedTilePrefab = defenseTilePrefab;
+                --numDefenseRings;
+            }
+            else if (numBarrierRings > 0)
+            {
+                //usedTile = barrierTilePrefab;
+                --numBarrierRings;
+            }
+            else
+            {
+                usedTilePrefab = attackTilePrefab;
+            }
+            
+            // Generate all tiles on the current ring
             foreach (Vector3Int hexCoordinates in grid.GetHexesOnRing(i))
             {
-                GameObject tileObject = Instantiate(tilePrefab, HexGrid.center, Quaternion.identity);
-                HexCoordinates hexCoord = tileObject.GetComponent<HexCoordinates>();
-                hexCoord.SetHexCoordinates(hexCoordinates);
+                if (usedTilePrefab)
+                {
+                    GameObject tileObject = Instantiate(usedTilePrefab, HexGrid.center, Quaternion.identity);
+                    HexCoordinates hexCoord = tileObject.GetComponent<HexCoordinates>();
+                    hexCoord.SetHexCoordinates(hexCoordinates);
+
+                    // Add to group
+                    if (tilesGroup)
+                    {
+                        tileObject.transform.parent = tilesGroup.transform;
+                    }
+                }
             }
         }
 
